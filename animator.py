89af29cdb1b4b5d74477880facf64602e75c512e
TheMaroco <maroco.miguel@gmail.com>
2022-06-04 19:08:45 +0100

    animation done with replicator solution now

--- animator.py
+++ animator.py
@@ -2,6 +2,7 @@
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+from system import plot
   
 def animate(parameter, data1, data2, axis_labels = [], plt_labels = []):
     x = []
@@ -12,9 +13,7 @@ def animate(parameter, data1, data2, axis_labels = [], plt_labels = []):
     ax.set_xlim(min(parameter), max(parameter))
     ax.set_ylim(min(min(data1), min(data2), max(max(data1), max(data2))))
 
-    
-
-    
+      
     def animation_function(i):
         ax.plot(parameter[:i], data1[:i], color = 'red', label = plt_labels[0])
         ax.plot(parameter[:i], data2[:i], color = 'blue',  label = plt_labels[1])
